ceab579bbaf51851199d406f9245a79205d25ebb
alikahwaji <ali.kahwaji@hotmail.com>
2019-08-28 15:19:27 +1200

    Update index.js
    Please follow my instructions

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import App from './components/app';
-
+//Please explain these?
 ReactDOM.render(<App />, document.querySelector('#root'))
